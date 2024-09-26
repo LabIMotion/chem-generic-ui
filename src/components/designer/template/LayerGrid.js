c52b91b926a6ca4268d0a24b9e951f242348c7fa
Chia-Lin Lin <chia-lin.lin@kit.edu>
2024-09-26 05:40:43 +0200

    refactor: display layer list via grid (#400)

--- src/components/designer/template/LayerGrid.js
+++ src/components/designer/template/LayerGrid.js
@@ -1,16 +1,12 @@
-import React, { useCallback, useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Button } from 'react-bootstrap';
 import { AgGridReact } from 'ag-grid-react';
 import LTooltip from '../../shared/LTooltip';
 import LayerManager from '../../../utils/desMgr';
 import ButtonConfirm from '../../fields/ButtonConfirm';
-import LayerGridFullWidth from './LayerGridFullWidth';
 
 const LayersGrid = ({ onLayerSelect, onLayerDelete, onLayerView }) => {
   const [rowData, setRowData] = useState([]);
-  const [expandedRowIndex, setExpandedRowIndex] = useState(null);
-  const [expandedRowHeight, setExpandedRowHeight] = useState(50); // Default height
-  const [gridHeight, setGridHeight] = useState(600);
   const gridRef = useRef(null); // Add a reference to the grid
 
   useEffect(() => {
@@ -22,63 +18,6 @@ const LayersGrid = ({ onLayerSelect, onLayerDelete, onLayerView }) => {
     };
     fetchLayers();
   }, []);
-
-  const updateGridLayout = useCallback(() => {
-    if (gridRef.current && gridRef.current.api) {
-      console.log('updateGridLayout');
-      // gridRef.current.api.resetRowHeights(); // This method is not needed when using dynamic row heights, and it's causing the conflict with Auto Row Height.
-      gridRef.current.api.onRowHeightChanged();
-      gridRef.current.api.refreshCells({ force: true });
-    }
-  }, []);
-
-  useEffect(() => {
-    updateGridLayout();
-  }, [expandedRowIndex, gridHeight, updateGridLayout]);
-
-  const handleHeightChange = useCallback(
-    (height) => {
-      setExpandedRowHeight(height + 20); // Add some padding
-      const newGridHeight = Math.max(600, rowData.length * 50 + height + 20);
-      setGridHeight(newGridHeight);
-    },
-    [rowData]
-  );
-
-  // Usage in getRowHeight
-  const getRowHeight = useCallback(
-    (params) => {
-      return params.node.rowIndex === expandedRowIndex ? expandedRowHeight : 50;
-    },
-    [expandedRowHeight, expandedRowIndex]
-  );
-
-  const isFullWidthRow = useCallback(
-    (params) => {
-      return params?.rowNode?.rowIndex === expandedRowIndex;
-    },
-    [expandedRowIndex]
-  );
-
-  const fullWidthRenderer = useCallback(
-    (params) => {
-      const { node, data } = params;
-      return (
-        // <div ref={contentRef} className="gu-full-width-panel">
-        <LayerGridFullWidth
-          node={node}
-          data={data}
-          onHeightChange={handleHeightChange}
-          onClose={() => {
-            setExpandedRowIndex(null);
-            updateGridLayout();
-          }}
-        />
-        // </div>
-      );
-    },
-    [handleHeightChange, updateGridLayout]
-  );
 
   const columnDefs = [
     {
@@ -161,20 +100,14 @@ const LayersGrid = ({ onLayerSelect, onLayerDelete, onLayerView }) => {
   return (
     <div
       className="ag-theme-alpine"
-      style={{ maxHeight: `${gridHeight}px`, width: '100%', overflow: 'auto' }}
-      // style={{ height: `${gridHeight}px`, width: '100%', overflow: 'auto' }}
+      style={{ height: '600px', width: '100%', overflow: 'auto' }}
     >
       <AgGridReact
         ref={gridRef}
         columnDefs={columnDefs}
         rowData={rowData}
-        domLayout="autoHeight"
-        // domLayout="normal"
-        isFullWidthRow={isFullWidthRow}
-        fullWidthCellRenderer={fullWidthRenderer}
-        getRowHeight={getRowHeight}
+        domLayout="normal"
         suppressAutoSize
-        animateRows
       />
     </div>
   );
